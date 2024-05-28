68109c4c4b96bf9cff3968b35b7934e60fcd2eea
sky-aman <129857913+sky-aman@users.noreply.github.com>
2024-05-28 08:55:58 +0000

    fundamental1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,95 @@
+
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob= "Programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob);
+
+// Chapter 2
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javaScriptIsFun = 'Yes';
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year, typeof year);
+// year = 1991;
+// console.log(year, typeof year);
+
+// console.log(null, typeof null);
+
+// Chapter 3 (LET, CONST and VAR)
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = "Dubey";
+console.log(lastName);
+
+
+// Assignment
+// Values and Variables
+// let country = "India";
+// let continent = "Asia";
+let population = 10;
+
+// console.log('country', country);
+// console.log('continent', continent);
+// console.log('population', population);
+
+// Data Types
+// let isIsland = false;
+// let language;
+
+// console.log(isIsland, typeof isIsland);
+// console.log(population, typeof population);
+// console.log(country, typeof country);
+// console.log(language, typeof language);
+
+// let, const and var
+let language;
+language = "hindi";
+const country = "India";
+const continent = "Asia";
+const isIsland = false;
+
+// isIsland = true;
+
+// Basic Operators
+console.log(population / 2);
+
+population += 1;
+console.log(population);
+
+let finlandPopulation = 6;
+console.log("Have more population than finland", population >  finlandPopulation);
+let averagePopulation = 33;
+console.log("Have less people than the average country", population < averagePopulation);
+
+let description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
+description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
+
